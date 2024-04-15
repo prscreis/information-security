3c661009edd6595ff805362f9d2260bb95a42479
Paulo Reis <prsc.reis@gmail.com>
2024-04-14 21:24:43 -0400

    Fixes and code organization

--- Testing.py
+++ Testing.py
@@ -1,4 +1,18 @@
-from KeyGeneration import generate_rsa_key_pair, save_key_to_file, encrypt_file, load_key_from_file, decrypt_file, generate_file_hash
+from cryptography.hazmat.primitives import serialization
+from IntegrityVerification import verify_file_integrity
+from KeyGeneration import generate_rsa_key_pair, encrypt_file, decrypt_file, generate_file_hash
+
+# save_key_to_file
+def save_key_to_file(key, file_path):
+    with open(file_path, "wb") as f:
+        f.write(key)
+
+# load_key_from_file
+def load_key_from_file(file_path):
+    with open(file_path, "rb") as f:
+        key = f.read()
+    return key
+
 
 def main():
     print("Secure File Transfer System")
@@ -12,25 +26,25 @@ def main():
             encoding=serialization.Encoding.PEM,
             format=serialization.PublicFormat.SubjectPublicKeyInfo
         ),
-        "public_key.pem"
+        "files/public_key.pem"
     )
 
     # Encrypt file using public key
-    encrypted_data = encrypt_file("file.txt", public_key)
-    with open("encrypted_file.bin", "wb") as f:
+    encrypted_data = encrypt_file("files/file.txt", public_key)
+    with open("files/encrypted_file.bin", "wb") as f:
         f.write(encrypted_data)
 
     # Decrypt file using private key
-    encrypted_data = load_key_from_file("encrypted_file.bin")
+    encrypted_data = load_key_from_file("files/encrypted_file.bin")
     decrypted_data = decrypt_file(encrypted_data, private_key)
-    with open("decrypted_file.txt", "wb") as f:
+    with open("files/decrypted_file.txt", "wb") as f:
         f.write(decrypted_data)
 
     # Generate hash of original file
-    original_hash = generate_file_hash("file.txt")
+    original_hash = generate_file_hash("files/file.txt")
 
     # Verify integrity of received file
-    is_integrity_verified = verify_integrity(original_hash, "decrypted_file.txt")
+    is_integrity_verified = verify_file_integrity(original_hash, "files/decrypted_file.txt")
     if is_integrity_verified:
         print("Integrity OK!")
     else:
